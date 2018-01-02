153f776c7a4b00aa5acc84e53e70e27bc754dfaa
ZzzHhhPpp <13715777132m@sina.cn>
2018-01-02 15:46:59 +0800

    shixun3

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/bean/CourseBean.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/bean/CourseBean.java
@@ -5,4 +5,9 @@ package cn.edu.gdmec.android.boxuegu.bean;
  */
 
 public class CourseBean {
+    public int id;
+    public String imgTitle;
+    public String title;
+    public String intro;
+    public String icon;
 }
